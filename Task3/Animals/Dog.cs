6e3f1551e07a663909cb131cb05ecd89c1ac526d
Nikita Voronezhskiy <voronezhskij_n_m@sc.vsu.ru>
2024-03-12 12:33:37 +0300

    Task3 init

---
+++ Task3/Animals/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            Speed = 0;
+        }
+
+        public override void Move()
+        {
+            Speed = Math.Min(Speed + 10, 50);
+
+        }
+
+        public override void Stand()
+        {
+            Speed = Math.Max(Speed - 10, 0);
+
+        }
+
+        public string Roar()
+        {
+            return "Gav gav!!!";
+        }
+    }
+}
--- Task3/MainWindow.xaml.cs
+++ Task3/MainWindow.xaml.cs
@@ -20,9 +20,82 @@ namespace Task3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PantherViewModel _viewModelPanther;
+        private DogViewModel _viewModelDog;
+        private TurtleViewModel _viewModelTurtle;
+
         public MainWindow()
         {
             InitializeComponent();
+            _viewModelPanther = new PantherViewModel();
+            _viewModelDog = new DogViewModel();
+            _viewModelTurtle = new TurtleViewModel();
+            DataContext = this;
+        }
+
+        private void MoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeToPantherDataContext();
+            _viewModelPanther.Move();
+        }
+
+        private void StandButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeToPantherDataContext();
+            _viewModelPanther.Stand();
+        }
+
+        private void RoarButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_viewModelPanther.Roar());
+        }
+
+        private void ClimbTreeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_viewModelPanther.ClimbTree());
+        }
+
+        private void ChangeToPantherDataContext()
+        {
+            DataContext = _viewModelPanther;
+        }
+
+        private void ChangeToDogDataContext()
+        {
+            DataContext = _viewModelDog;
+        }
+
+        private void ChangeToTurtleDataContext()
+        {
+            DataContext = _viewModelTurtle;
+        }
+
+        private void MoveButton_ClickDog(object sender, RoutedEventArgs e)
+        {
+            ChangeToDogDataContext();
+            _viewModelDog.Move();
+        }
+
+        private void StandButton_ClickDog(object sender, RoutedEventArgs e)
+        {
+            ChangeToDogDataContext();
+            _viewModelDog.Stand();
+        }
+
+        private void RoarButton_ClickDog(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_viewModelDog.Roar());
+        }
+        private void MoveButton_ClickTurtle(object sender, RoutedEventArgs e)
+        {
+            ChangeToTurtleDataContext();
+            _viewModelTurtle.Move();
+        }
+
+        private void StandButton_ClickTurtle(object sender, RoutedEventArgs e)
+        {
+            ChangeToTurtleDataContext();
+            _viewModelTurtle.Stand();
         }
     }
 }
